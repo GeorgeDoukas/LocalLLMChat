c1de3de49c90cf0b2dd3fefeba0564fd1ea18a87
Doukas George <gdoukas@intracom-telecom.com>
2024-10-09 12:08:36 +0300

    changed Dialog to Exchange

--- chat/models.py
+++ chat/models.py
@@ -9,15 +9,25 @@ class CallSession(models.Model):
 
     # This will give you the list of dialogs belonging to a session
     def get_dialogs(self):
-        return self.dialogs.all()
+        return self.exchanges.all()
 
-class Dialog(models.Model):  # Renamed Message to Dialog
-    session = models.ForeignKey(CallSession, on_delete=models.CASCADE, related_name='dialogs')
-    user_input = models.TextField(blank=True, null=True)
-    bot_response = models.TextField(blank=True, null=True)
-    user_audio = models.FileField(upload_to='user_audio/', blank=True, null=True)  # Store user's audio clip
-    bot_audio = models.FileField(upload_to='bot_audio/', blank=True, null=True)  # Store bot's audio response
+class Exchange(models.Model):  # Renamed Message to Dialog
+    SPEAKER_CHOICES = [
+        ("U", "User"),
+        ("A", "Agent"),
+    ]
+    session = models.ForeignKey(CallSession, on_delete=models.CASCADE, related_name='exchanges')
+    speaker = models.CharField(max_length=1, choices=SPEAKER_CHOICES, default="U")
+    input = models.TextField(blank=True, null=True)
+    response = models.TextField(blank=True, null=True)
+    audio = models.FileField(upload_to='audio/user/', blank=True, null=True)  # Store user's audio clip
     timestamp = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f"Dialog at {self.timestamp} - Session {self.session.id} user: {self.user_input} bot: {self.bot_response}"
+        return f"Exchange from {self.get_speaker()} at {self.timestamp} - Session - {self.session.id} {self.input} : {self.response}"
+
+    def get_speaker(self):
+        for choice in self.SPEAKER_CHOICES:
+            if choice[0] == self.speaker:
+                return choice[1]
+        return "Unknown"
